ae680b55885ff8c9f5760dd0e26a353bf004739b
Moazam Ali <mozalikorai@gmail.com>
2023-12-20 01:39:42 +0500

    Finished Gatby Tutorial Part 3

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -2,10 +2,12 @@ import * as React from "react";
 import type { HeadFC, PageProps } from "gatsby";
 import { Link } from "gatsby";
 import Layout from "../components/layout";
+import { StaticImage } from "gatsby-plugin-image";
 
 const AboutPage: React.FC<PageProps> = () => {
 	return (
 		<Layout pageTitle="About Me">
+			<StaticImage alt="People in a crowd" src="../images/people.webp" />
 			<p>
 				Hi there! I'm the proud creator of this site, which I built with
 				Gatsby.
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,15 @@
 import * as React from "react";
 import type { HeadFC, PageProps } from "gatsby";
-import { Link } from "gatsby";
 import Layout from "../components/layout";
+import { StaticImage } from "gatsby-plugin-image";
 
 const IndexPage: React.FC<PageProps> = () => {
 	return (
 		<Layout pageTitle="Home Page">
+			<StaticImage
+				alt="Clifford, a reddish-brown pitbull, posing on a couch and looking stoically at the camera"
+				src="https://pbs.twimg.com/media/E1oMV3QVgAIr1NT?format=jpg&name=large"
+			/>
 			<p>I'm making this by following the Gatsby Tutorial.</p>
 		</Layout>
 	);
